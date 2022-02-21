2ad2c3dd6efa927a2b33ce5c0522e2882e150dcc
RohanXKapur <rohankapurdev@outlook.com>
2022-02-21 22:40:15 +0800

    add seed check to transfer_merchant_authority

--- programs/recurring/src/instructions/transfer_merchant_authority.rs
+++ programs/recurring/src/instructions/transfer_merchant_authority.rs
@@ -6,8 +6,11 @@ pub struct TransferMerchantAuthority<'info> {
     #[account(constraint = payer.key() == merchant_authority.current_authority @ ErrorCode::IncorrectAuthority)]
     pub payer: Signer<'info>,
 
-    #[account(mut)]
+    #[account(mut, seeds = [b"merchant_authority", merchant_authority.key().as_ref(), init_authority.key().as_ref()], bump)]
     pub merchant_authority: Account<'info, MerchantAuthority>,
+
+    #[account(constraint = init_authority.key() == merchant_authority.init_authority @ ErrorCode::IncorrectInitAuthority)]
+    pub init_authority: UncheckedAccount<'info>,
 
     pub proposed_authority: UncheckedAccount<'info>,
 }
