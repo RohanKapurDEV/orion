170e701573a6ec2df8df0d66bd3520792fc2c32b
RohanXKapur <rohankapurdev@outlook.com>
2022-02-21 14:37:31 +0800

    setup testing environment

--- tests/recurring.ts
+++ tests/recurring.ts
@@ -1,17 +1,16 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { Recurring } from '../target/types/recurring';
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { Recurring } from "../target/types/recurring";
 
-describe('recurring', () => {
-
+describe("recurring", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.Recurring as Program<Recurring>;
 
-  it('Is initialized!', async () => {
+  it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+    // const tx = await program.rpc.initialize({});
+    // console.log("Your transaction signature", tx);
   });
 });
