97377a084883ad2aa921096411d6d06587c26946
RohanXKapur <rohankapurdev@outlook.com>
2022-02-22 00:05:23 +0800

    add ixs to module

--- programs/recurring/src/instructions/mod.rs
+++ programs/recurring/src/instructions/mod.rs
@@ -1,9 +1,11 @@
+pub mod accept_merchant_authority;
 pub mod collect_payment;
 pub mod initialize_merchant_authority;
 pub mod initialize_payment_config;
 pub mod initialize_payment_metadata;
 pub mod transfer_merchant_authority;
 
+pub use accept_merchant_authority::*;
 pub use collect_payment::*;
 pub use initialize_merchant_authority::*;
 pub use initialize_payment_config::*;
