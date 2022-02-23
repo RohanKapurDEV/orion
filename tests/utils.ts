b43a4c960f6ff69f098e95c2df51a465066f7992
RohanXKapur <rohankapurdev@outlook.com>
2022-02-23 14:28:14 +0800

    wrong repo lmao

--- tests/utils.ts
+++ tests/utils.ts
@@ -15,7 +15,3 @@ export const bnTo16 = (bn: BN): Uint8Array => {
 export const bnTo1 = (bn: BN): Uint8Array => {
   return Buffer.from([...bn.toArray("le", 1)]);
 };
-
-export const TOKEN_METADATA = new PublicKey(
-  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
-);
