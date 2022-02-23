c79fc3cb59d93d37f68b8738d108f488b8c79873
RohanXKapur <rohankapurdev@outlook.com>
2022-02-23 14:27:00 +0800

    add token_metadata program ID to utils export

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,9 +1,21 @@
 import BN from "bn.js";
+import { PublicKey } from "@solana/web3.js";
 
+// .to_le_bytes() for u64
 export const bnTo8 = (bn: BN): Uint8Array => {
   return Buffer.from([...bn.toArray("le", 8)]);
 };
 
+// .to_le_bytes() for u16
+export const bnTo16 = (bn: BN): Uint8Array => {
+  return Buffer.from([...bn.toArray("le", 2)]);
+};
+
+// .to_le_bytes() for u8
 export const bnTo1 = (bn: BN): Uint8Array => {
   return Buffer.from([...bn.toArray("le", 1)]);
 };
+
+export const TOKEN_METADATA = new PublicKey(
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
