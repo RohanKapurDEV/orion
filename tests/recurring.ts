0c7a7bcea02091b5e6fbd2cd735e243cb4a4a57b
RohanXKapur <rohankapurdev@outlook.com>
2022-02-23 23:20:26 +0800

    add initialize_payment_config test

--- tests/recurring.ts
+++ tests/recurring.ts
@@ -4,20 +4,22 @@ import { Recurring } from "../target/types/recurring";
 import {
   TOKEN_PROGRAM_ID,
   ASSOCIATED_TOKEN_PROGRAM_ID,
+  createMint,
 } from "@solana/spl-token";
-import { SystemProgram } from "@solana/web3.js";
+import { PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
 import { bnTo8, bnTo1 } from "./utils";
 import BN from "bn.js";
+import { assert } from "chai";
 
 describe("recurring", async () => {
-  // Configure the client to use the local cluster.
   const provider = anchor.Provider.env();
 
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.Recurring as Program<Recurring>;
 
-  const index = 0;
+  const mintDecimals = 6;
+  const merchantAuthorityIndex = 0;
   const payer = anchor.web3.Keypair.generate();
   const authority = anchor.web3.Keypair.generate();
 
@@ -25,27 +27,50 @@ describe("recurring", async () => {
     await anchor.web3.PublicKey.findProgramAddress(
       [
         Buffer.from("merchant_authority"),
-        bnTo1(new BN(index)),
+        bnTo1(new BN(merchantAuthorityIndex)),
         authority.publicKey.toBytes(),
       ],
       program.programId
     );
 
-  console.log("Payer public key: " + payer.publicKey.toString());
-  console.log("Authority public key: " + authority.publicKey.toString());
-  console.log("MerchantAuthority public key: " + merchantAuthority.toString());
+  const paymentConfigIndex = 0;
+  const [paymentConfig, _paymentConfigBump] =
+    await anchor.web3.PublicKey.findProgramAddress(
+      [
+        Buffer.from("payment_config"),
+        bnTo1(new BN(paymentConfigIndex)),
+        merchantAuthority.toBytes(),
+      ],
+      program.programId
+    );
+  const paymentTokenAccount = anchor.web3.Keypair.generate(); // PaymentConfig payment_token_account
+
+  let paymentMint = PublicKey.default;
 
   // Airdrop some sweet, sweet lamports first
   before(async () => {
     await provider.connection.confirmTransaction(
-      await provider.connection.requestAirdrop(payer.publicKey, 10_000_000_000),
+      await provider.connection.requestAirdrop(payer.publicKey, 10_000_000_000), // 10 SOL
+      "confirmed"
+    );
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(
+        authority.publicKey,
+        10_000_000_000
+      ), // 10 SOL
       "confirmed"
     );
   });
 
+  console.log("Payer public key: " + payer.publicKey.toString());
+  console.log("Authority public key: " + authority.publicKey.toString());
+  console.log("MerchantAuthority public key: " + merchantAuthority.toString());
+  console.log("PaymentConfig public key: " + paymentConfig.toString());
+  console.log("PaymentMint public key: " + paymentMint.toString());
+
   it("Create MerchantAuthority account!", async () => {
-    let tx = await program.methods
-      .initializeMerchantAuthority(index)
+    await program.methods
+      .initializeMerchantAuthority(merchantAuthorityIndex)
       .accounts({
         payer: payer.publicKey,
         merchantAuthority: merchantAuthority,
@@ -55,15 +80,55 @@ describe("recurring", async () => {
       .signers([payer])
       .rpc();
 
-    console.log("initialize_merchant_account tx sig: " + tx);
-
     let merchant_authority_account =
       await program.account.merchantAuthority.fetch(merchantAuthority);
-
-    console.log(merchant_authority_account);
+    console.log(merchant_authority_account.currentAuthority.toString());
   });
 
-  it("Create PaymentConfig account!", async () => {});
+  it("Create PaymentConfig account!", async () => {
+    let x = await createMint(
+      provider.connection,
+      payer,
+      payer.publicKey,
+      payer.publicKey,
+      mintDecimals
+    );
+    paymentMint = x;
+
+    let paymentConfigParams = {
+      index: 0,
+      minimumAmountToDelegate: 10000 * (10 ^ mintDecimals),
+      spacingPeriod: 5,
+      collectOnInit: true,
+      amountToCollectOnInit: 100 * (10 ^ mintDecimals),
+      amountToCollectPerPeriod: 100 * (10 ^ mintDecimals),
+    };
+
+    let tx = await program.methods
+      .initializePaymentConfig(
+        paymentConfigParams.index,
+        new BN(paymentConfigParams.minimumAmountToDelegate),
+        new BN(paymentConfigParams.spacingPeriod),
+        paymentConfigParams.collectOnInit,
+        new BN(paymentConfigParams.amountToCollectOnInit),
+        new BN(paymentConfigParams.amountToCollectPerPeriod)
+      )
+      .accounts({
+        payer: authority.publicKey,
+        paymentConfig: paymentConfig,
+        merchantAuth: merchantAuthority,
+        paymentMint: paymentMint,
+        paymentTokenAccount: paymentTokenAccount.publicKey,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+      })
+      .signers([authority, paymentTokenAccount])
+      .rpc();
+
+    console.log(tx);
+  });
+
   it("Create PaymentMetadata account!", async () => {});
   it("Collect payment from PaymentMetadata account!", async () => {});
   it("Transfer MerchantAuthority account!", async () => {});
