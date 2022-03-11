7f6558b6ff234455047d8c4ff2ddc3e5cfa90b2d
RohanXKapur <rohankapurdev@outlook.com>
2022-03-12 05:16:20 +0800

    add accept merch auth event

--- programs/recurring/src/instructions/accept_merchant_authority.rs
+++ programs/recurring/src/instructions/accept_merchant_authority.rs
@@ -1,6 +1,14 @@
 use crate::{error::*, state::*};
 use anchor_lang::prelude::*;
 
+#[event]
+pub struct AcceptMerchantAuthorityEvent {
+    pub merchant_authority: Pubkey,
+    pub old_authority: Pubkey,
+    pub new_authority: Pubkey,
+    pub timestamp: i64,
+}
+
 #[derive(Accounts)]
 #[instruction(index: u8)]
 pub struct AcceptMerchantAuthority<'info> {
@@ -18,10 +26,20 @@ pub fn handler(ctx: Context<AcceptMerchantAuthority>, _index: u8) -> ProgramResu
     let merchant_authority = &mut ctx.accounts.merchant_authority;
     let new_authority = &mut ctx.accounts.payer;
 
-    let previous_authority = merchant_authority.current_authority; // for use in event
+    let previous_authority = merchant_authority.current_authority;
 
     merchant_authority.current_authority = new_authority.key();
     merchant_authority.pending_authority = Pubkey::default();
+
+    let clock = Clock::get()?;
+    let unix_timestamp = clock.unix_timestamp;
+
+    emit!(AcceptMerchantAuthorityEvent {
+        merchant_authority: merchant_authority.key(),
+        new_authority: new_authority.key(),
+        old_authority: previous_authority,
+        timestamp: unix_timestamp
+    });
 
     Ok(())
 }
