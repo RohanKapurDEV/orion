9c18c1841931c43b721e53398e60bb6baf3d3f1b
RohanXKapur <rohankapurdev@outlook.com>
2022-04-28 02:12:37 +0800

    add init ma account to client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,112 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fmt::Display, rc::Rc, str::FromStr};
+
+use anchor_client::{
+    solana_sdk::{
+        commitment_config::CommitmentConfig, pubkey::Pubkey, signature::Keypair, signer::Signer,
+    },
+    Client, ClientError, Cluster,
+};
+use dotenv::dotenv;
+use recurring::accounts as recurring_accounts;
+use recurring::instruction as recurring_ixs;
+use recurring::state::*;
+
+mod utils;
+
+use utils::*;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    // Note that this env var should be a base58 string representation of a private key
+    let payer_private_key = dotenv::var("PAYER_PRIVATE_KEY").unwrap();
+    let payer_signer = Keypair::from_base58_string(&payer_private_key);
+    let payer_pubkey = payer_signer.pubkey();
+
+    let network = NetworkSelector::Devnet;
+    let rpc = network.fetch_rpc();
+
+    let cluster = Cluster::Custom(rpc.0.to_string(), rpc.1.to_string());
+    let client = Client::new_with_options(
+        cluster,
+        Rc::new(payer_signer),
+        CommitmentConfig::processed(),
+    );
+
+    let res = initialize_merchant_authority(
+        &client,
+        payer_pubkey.clone(),
+        payer_pubkey.clone(),
+        payer_private_key.clone(),
+    )
+    .await;
+
+    match res {
+        Ok(_value) => {}
+        Err(e) => {
+            println!("{}", e)
+        }
+    }
+}
+
+pub async fn initialize_merchant_authority(
+    client: &Client,
+    payer: Pubkey,
+    authority: Pubkey,
+    payer_private_key: String,
+) -> Result<bool, ClientError> {
+    let program_id_pubkey = Pubkey::from_str(PROGRAM_ID).unwrap();
+    let system_program_pubkey = Pubkey::from_str(SYSTEM_PROGRAM).unwrap();
+    let payer_signer = Keypair::from_base58_string(&payer_private_key);
+
+    let program = client.program(program_id_pubkey);
+    let (merchant_authority, _bump) = Pubkey::find_program_address(
+        &[
+            b"merchant_authority",
+            &MERCHANT_AUTHORITY_INDEX.to_le_bytes(),
+            &authority.to_bytes(),
+        ],
+        &program_id_pubkey,
+    );
+
+    let accounts = recurring_accounts::InitializeMerchantAuthority {
+        payer: payer,
+        merchant_authority,
+        authority,
+        system_program: system_program_pubkey,
+    };
+    let params = recurring_ixs::InitializeMerchantAuthority {
+        index: MERCHANT_AUTHORITY_INDEX,
+    };
+
+    program
+        .request()
+        .signer(&payer_signer)
+        .accounts(accounts)
+        .args(params)
+        .send()?;
+
+    let merch_auth: MerchantAuthority = program.account(merchant_authority)?;
+
+    println!("Current authority: {}", merch_auth.current_authority);
+    println!("Index: {}", merch_auth.index);
+
+    Ok(true)
+}
+
+pub enum NetworkSelector {
+    Mainnet,
+    Devnet,
+}
+
+impl NetworkSelector {
+    fn fetch_rpc(self) -> (String, String) {
+        match self {
+            NetworkSelector::Mainnet => return ("".to_string(), "".to_string()),
+            NetworkSelector::Devnet => {
+                return (DEVNET_RPC_HTTP.to_string(), DEVNET_RPC_WS.to_string())
+            }
+        }
+    }
 }
--- client/src/utils.rs
+++ client/src/utils.rs
@@ -1,5 +1,10 @@
 // Some useful constants
-pub const PROGRAM_ID: &str = "";
-pub const PAYER: &str = "";
-pub const TOKEN_PROGRAM: &str = "";
-pub const ASSOCIATED_TOKEN_PROGRAM: &str = "";
+pub const PROGRAM_ID: &str = "CFHiFGAChg829XSFBRhswft7Vnmc9tQdR3Esiqcxmeef";
+pub const PAYER: &str = "4puafxtL1437aibBy4pCteADWjja9aQvygD9LhkwRMG5";
+pub const TOKEN_PROGRAM: &str = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+pub const ASSOCIATED_TOKEN_PROGRAM: &str = "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL";
+pub const RENT: &str = "SysvarRent111111111111111111111111111111111";
+pub const SYSTEM_PROGRAM: &str = "11111111111111111111111111111111";
+pub const DEVNET_RPC_HTTP: &str = "https://api.devnet.solana.com";
+pub const DEVNET_RPC_WS: &str = "wss://api.devnet.solana.com";
+pub const MERCHANT_AUTHORITY_INDEX: u8 = 0;
