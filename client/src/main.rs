21cfadf4d90ef4e0ff0650e02f2b242f235f422e
RohanXKapur <rohankapurdev@outlook.com>
2022-04-30 02:23:40 +0800

    hook up ix to handler

--- client/src/main.rs
+++ client/src/main.rs
@@ -23,9 +23,32 @@ async fn main() {
                 let res = initialize_merchant_authority(&client, keypair_path, params.index).await;
 
                 match res {
-                    Ok(_) => {
-                        println!("Successful tx")
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("{}", e.to_string())
                     }
+                }
+            }
+            EntityType::InitPaymentConfig(params) => {
+                let network = params.network;
+                let keypair_path = params.keypair_path;
+
+                let client = build_client(keypair_path.clone(), network);
+                let res = initialize_payment_config(
+                    &client,
+                    keypair_path,
+                    params.merchant_authority,
+                    params.payment_mint,
+                    params.index,
+                    params.spacing_period,
+                    params.collect_on_init,
+                    params.amount_to_collect_on_init,
+                    params.amount_to_collect_per_period,
+                )
+                .await;
+
+                match res {
+                    Ok(_) => {}
                     Err(e) => {
                         println!("{}", e.to_string())
                     }
