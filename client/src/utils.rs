76eb8b5b5a8e6c6f9d93eb8e0694f0a776713cc1
RohanXKapur <rohankapurdev@outlook.com>
2022-04-30 08:19:56 +0800

    cleanup

--- client/src/utils.rs
+++ client/src/utils.rs
@@ -7,7 +7,6 @@ use std::fs;
 use std::rc::Rc;
 
 pub const PROGRAM_ID: &str = "CFHiFGAChg829XSFBRhswft7Vnmc9tQdR3Esiqcxmeef";
-pub const PAYER: &str = "4puafxtL1437aibBy4pCteADWjja9aQvygD9LhkwRMG5";
 pub const TOKEN_PROGRAM: &str = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
 pub const ASSOCIATED_TOKEN_PROGRAM: &str = "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL";
 pub const RENT: &str = "SysvarRent111111111111111111111111111111111";
